a2967e774ec87af3dc6c1d0fed2e6cd41b9df7fe
IrakliTabagari <iraklitabag89@gmail.com>
2024-05-27 12:54:45 +0400

    validation_errors

--- Client/src/app/shared/server-validation-error.service.ts
+++ Client/src/app/shared/server-validation-error.service.ts
@@ -9,90 +9,16 @@ export class ServerValidationErrorService {
 
   constructor() { }
 
-  getError(field: string, errorResponse: HttpErrorResponse): string {
-    if (errorResponse.error == null || errorResponse.error.errors == null)
-      return "";
-
-    const errors = { };
-    for (const key in errorResponse.error.errors) {
-      if (Object.prototype.hasOwnProperty.call(errorResponse.error.errors, key)){
-        errors[key] = errorResponse.error.errors[key];
-      }
-    }
-    return "";
-  }
-
-
-
   renderServerErrors(form: FormGroup, response: HttpErrorResponse) {
-    if (response.error.errors == null) {
-      return;
-    }
-
-    response.error.errors.forEach((element: any) => {
-      // field errors are handled here, different errors should be handled globally
-      if (element.type === 'field-validation-error') {
-        const fieldName = element.field;
-        const message = element.message;
-        if (form == null || !this.hasFieldName(form, fieldName)) {
-        } else {
-          this.setFieldError(form, fieldName, message);
+    let serverErrors = response.error.errors;
+    for (let key in serverErrors) {
+      if (serverErrors.hasOwnProperty(key)) {
+        let control = form.get(key);
+        if (control) {
+          control.setErrors({serverError: serverErrors[key]});
         }
-      } else {
-        const message = element.message;
       }
-    });
-  }
-
-  hasWrongValue(form: FormGroup, fieldName: string): boolean {
-    return this.getFieldErrors(form, fieldName).length > 0;
-  }
-
-  // a field is correct only if it is filled and have no errors
-  hasCorrectValue(form: FormGroup, fieldName: string): boolean {
-    const control = this.findFieldControl(form, fieldName);
-    // field found && user changed it && it doesn't hold a wrong value
-    const isCorrect = control && !control.pristine && !this.hasWrongValue(form, fieldName);
-
-    return isCorrect;
-  }
-
-  // right now we are rendering one error only for a field
-  getFieldError(form: FormGroup, fieldName: string): string {
-    return this.getFieldErrors(form, fieldName)[0];
-  }
-
-  getFieldErrors(form: FormGroup, fieldName: string): string[] {
-    const control = this.findFieldControl(form, fieldName);
-    if (control && control.touched && control.errors) {
-      return this.getErrors(control);
-    } else {
-      return [];
     }
   }
 
-  getErrors(control: AbstractControl): string[] {
-    return Object.keys(control.errors)
-      .filter((error: any) => control.errors[error])
-      .map((error: any) => {
-        const params = control.errors[error];
-        return error;
-      });
-  }
-
-  private hasFieldName(form: FormGroup, fieldName: string): boolean {
-    const control = this.findFieldControl(form, fieldName);
-    return control != null;
-  }
-
-  private setFieldError(form: FormGroup, fieldName: string, message: string) {
-    const control = this.findFieldControl(form, fieldName);
-    // TODO: find a better way to set the error message
-    const errors = { [message]: true };
-    control.setErrors(errors);
-  }
-
-  private findFieldControl(form: FormGroup, fieldName: string): AbstractControl {
-    return form.get(fieldName);
-  }
 }
--- Server/Server.Api/Models/Login/Request/LoginWithEmailAndPasswordRequestModel.cs
+++ Server/Server.Api/Models/Login/Request/LoginWithEmailAndPasswordRequestModel.cs
@@ -4,12 +4,12 @@ namespace Server.Api.Models.Login.Request;
 
 public record class LoginWithEmailAndPasswordRequestModel
 {
-    [Required]
-    [MinLength(3)]
+    [Required(ErrorMessage = "Email is required")]
+    [MinLength(3, ErrorMessage = "Email length must be minimum 3 letters")]
     [EmailAddress(ErrorMessage = "Email address is not valid.")]
     public required string Email { get; set; }
     
-    [Required]
-    [MinLength(3)]
+    [Required(ErrorMessage = "Password is required")]
+    [MinLength(3, ErrorMessage = "Password length must be minimum 3 letters")]
     public required string Password { get; set; }
 }
